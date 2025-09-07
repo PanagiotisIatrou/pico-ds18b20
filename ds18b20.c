8575af5ad81e5bf640d6ea8b9d6cb4c2ce47b4ea
Panagiotis Iatrou <iatrou.panag@gmail.com>
2025-09-07 18:55:23 +0300

    Write ROM command

--- ds18b20.c
+++ ds18b20.c
@@ -3,6 +3,19 @@
 
 const int data_pin = 0;
 
+void write_value(bool value) {
+    gpio_set_dir(data_pin, GPIO_OUT);
+    gpio_put(data_pin, 0);
+    if (value) {
+        sleep_us(8);
+        gpio_set_dir(data_pin, GPIO_IN);
+        sleep_us(52);
+    } else {
+        sleep_us(60);
+        gpio_set_dir(data_pin, GPIO_IN);
+    }
+}
+
 int main()
 {
     stdio_init_all();
@@ -28,13 +41,62 @@ int main()
     gpio_put(data_pin, 0);
     sleep_us(500);
 
-    // Wait for presense pulse
+    // Wait for presence pulse
     gpio_set_dir(data_pin, GPIO_IN);
     sleep_us(70); // 10uS for rising edge + 15-60uS for slave waiting
-    bool presense_pulse = gpio_get(data_pin);
-    if (!presense_pulse) {
-        printf("Detected presense pulse!!\n");
-    } else {
-        printf("Nooooo! :(\n");
+    int max_presence_pulse_wait_time = 240;
+    bool detected_presence_pulse = false;
+    uint32_t presence_pulse_start_time = to_us_since_boot(get_absolute_time());
+    while (!detected_presence_pulse) {
+        if (to_us_since_boot(get_absolute_time()) - presence_pulse_start_time > 240) {
+            break;
+        }
+
+        if (gpio_get(data_pin) == 0) {
+            detected_presence_pulse = true;
+            break;
+        }
+
+        sleep_us(1);
     }
+
+    if (!detected_presence_pulse) {
+        printf(":( Did not detect presence pulse!\n");
+        return 0;
+    }
+
+    printf("Detected presence pulse!\n");
+
+    // Wait for presence pulse to end
+    bool detected_presence_pulse_end = false;
+    presence_pulse_start_time = to_us_since_boot(get_absolute_time());
+    while (gpio_get(data_pin) == 0) {
+        if (to_us_since_boot(get_absolute_time()) - presence_pulse_start_time > 240) {
+            break;
+        }
+
+        if (gpio_get(data_pin) == 1) {
+            detected_presence_pulse_end = true;
+            break;
+        }
+
+        sleep_us(1);
+    }
+
+    if (!detected_presence_pulse_end) {
+        printf(":( Did not detect presence pulse end!\n");
+        return 0;
+    }
+
+    printf("Detected presence pulse end!\n");
+
+    // Send ROM command
+    char command[8] = "00110011";
+    for (int i = 7; i >= 0; i--) {
+        bool bit = command[i] == "1";
+        write_value(bit);
+        sleep_us(1);
+    }
+
+    printf("Wrote ROM command!\n");
 }
