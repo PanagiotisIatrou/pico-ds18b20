f8b9dd43fc991d473d38b065d187ebb942cc8af2
Panagiotis Iatrou <iatrou.panag@gmail.com>
2025-09-10 00:26:20 +0300

    Added Rom struct comparator function

--- src/rom.hpp
+++ src/rom.hpp
@@ -7,3 +7,19 @@ struct Rom {
     uint8_t serial_number[6];
     uint8_t crc_code;
 };
+
+inline bool operator==(const Rom& lhs, const Rom& rhs) {
+    if (lhs.family_code != rhs.family_code) {
+        return false;
+    }
+    for (int i = 0; i < 6; i++) {
+        if (lhs.serial_number[i] != rhs.serial_number[i]) {
+            return false;
+        }
+    }
+    if (lhs.crc_code != rhs.crc_code) {
+        return false;
+    }
+
+    return true;
+}
