bd0419d6d6cec06e9fd5d20d03a0e61cc53ec61f
Panagiotis Iatrou <iatrou.panag@gmail.com>
2025-09-14 14:28:00 +0300

    Added documentation for Rom

--- src/rom.hpp
+++ src/rom.hpp
@@ -9,24 +9,63 @@ private:
     uint8_t m_crc_code;
 
 public:
+    /**
+     * Creates an empty Rom (64 bits set to 0).
+     */
     Rom();
 
+    /**
+     * Creates a Rom and fills it with the parameters.
+     * @param family_code The 8-bit family code of the device.
+     * @param serial_number The 48-bit serial number of the device.
+     * @param crc_code The 8-bit CRC code of the device.
+     */
     Rom(uint8_t family_code, uint8_t serial_number[6], uint8_t crc_code);
 
+    /**
+     * @return The 8-bit family code of the device.
+     */
     uint8_t get_family_code();
 
+    /**
+     * @param index The index of the 6 bytes of the serial number (index 0 is the LSB)
+     * @return The 48-bit serial number of the device.
+     */
     uint8_t get_serial_number(int index);
 
+    /**
+     * @return The 8-bit CRC code of the device.
+     */
     uint8_t get_crc_code();
 
+    /**
+     * Checks if the Rom contains all zeros.
+     * @return true if all zeros, false if not.
+     */
     bool is_empty();
 
+    /**
+     * Calculates the CRC code from the family code and the serial number and checks if
+     * it matches the Rom CRC code.
+     * @return true if the match, false if not.
+     */
     bool has_valid_crc();
 
-    static uint64_t encode_rom(Rom encoded_rom);
+    /**
+     * @param decoded_rom A Rom object
+     * @return The 64-bit representation of decoded_rom
+     */
+    static uint64_t encode_rom(Rom decoded_rom);
 
+    /**
+     * @param encoded_rom A 64-bit representation of a Rom
+     * @return The Rom object representation of encoded_rom
+     */
     static Rom decode_rom(uint64_t encoded_rom);
 
+    /**
+     * Checks if 2 Roms are equal
+     */
     bool operator==(const Rom& other) const {
         if (m_family_code != other.m_family_code) {
             return false;
@@ -43,6 +82,9 @@ public:
         return true;
     }
 
+    /**
+     * Checks if 2 Roms are not equal
+     */
     bool operator!=(const Rom& other) {
         return !(*this == other);
     }
